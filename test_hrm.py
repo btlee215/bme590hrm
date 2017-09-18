abd0bca852849f6c873881e181c1d12ee258fb90
Sameer Pandhare <sammyp@Sameers-MacBook-Pro.local>
2017-09-17 22:46:04 -0400

    First attempt at unit test

--- test_hrm.py
+++ test_hrm.py
@@ -1,2 +1,17 @@
-def test_avg()
+import hrmfunction
 
+def test_avg()
+	assert hrmfunction.average(50,60)== 
+	assert hrmfunction.average(60,70)== 
+
+def test_instant
+	assert hrmfunction.instant(50,60)==
+	assert hrmfunction.instant(60,70)==
+
+def test_tachbrady
+	assert hrmfunction.tachbrady() == "Tachycardia"
+	assert hrmfunction.tachbrady() == "Bradycardia" 
+
+def test_inputs 
+	assert hrmfunction.average () == "Please enter an integer input"
+ 
