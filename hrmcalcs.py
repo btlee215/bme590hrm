cbb0289856f94fc866d199ccd673abba4d2aeef5
Alice <alice@Alices-MacBook-Pro.local>
2017-09-20 18:18:58 -0400

    This closes #2, closes #3, closes #5 . Both hrmread and hrmcalcs were altered

--- hrmcalcs.py
+++ hrmcalcs.py
@@ -1,29 +1,33 @@
 import hrmread
 import numpy as np
 
-peakvalues = hrmread.main()
-timebeat = np.diff(peakvalues)
-instantHR = []
-
 
 def hrminstant(peakvalues):
+    instantHR = []
     for i in peakvalues:
         instantHR.append(60/timebeat[i])
     return instantHR
 
-def hrmaverage(minutes):
-    seconds = 60*minutes
-    placeholder = seconds/np.array(peakvalues)
-    index = (np.abs(placeholder-1)).argmin()
-    timevals = timebeat[:index]
-    avetimebeat = np.average(timevals)
-    averageHR = 60/avetimebeat
+
+def hrmaverage(minutes = 2, timebeat):
+    averageHR = []
+    seconds = 60*minutes # range of seconds over which to average
+    # placeholder = seconds/np.array(peakvalues)
+    for i in peakvalues:
+        start_index =  np.argmax(peak_values > (peakvalues[i] - seconds))
+        end_index = np.argmax(peak_values > peak_values[i]) - 1
+        timevals = timebeat[start_index:end_index]
+        avetimebeat = np.average(timevals)
+        averageHR[i] = 60/avetimebeat
     return averageHR
 
-def maincalcs():
-    instantHR = hrminstant(peakvalues)
-    averageHR = hrmaverage(minutes)
 
+def maincalcs():
+    peakvalues = hrmread.main()
+    timebeat = np.diff(peakvalues)
+    instantHR = hrminstant(peakvalues)
+    averageHR = hrmaverage(minutes, timebeat)
+    return instantHR, averageHR
 
 if __name__ == "main":
     maincalcs()
--- hrmread.py
+++ hrmread.py
@@ -2,24 +2,24 @@
 # this file should read in the csv file, sort into time and voltage
 # should also output vector of times of each heartbeat
 import csv
+import numpy as np
 
 
 def read_ecg(file='Test_ECG.csv'):
-    with open (file) as ecg_Data_File:
-        ecg_reader = csv.reader(ecg_Data_File)
-        # Extract data
-        # data = get my data...
-
-    # return numpy data
-    return ecg_reader
-
-
-def find_range(ecg_reader, peakthresh = 0.7, basethresh = 0.1):
     time = []
     voltage = []
-    for row in ecg_reader:
-        time.append(row[0])
-        voltage.append(row[1])
+    if file.endswith('.csv'):
+        with open (file) as ecg_Data_File:
+            ecg_reader = csv.reader(ecg_Data_File)
+            next(ecg_reader)
+            for row in ecg_reader:
+                time.append(row[0])
+                voltage.append(row[1])
+        return time, voltage
+    # else need to insert error message here for if not a csv file!
+
+
+def find_range(time, voltage, peakthresh = 0.7, basethresh = 0.1):
     toggle_peak_status = 0
     import statistics
     peak_times = []
@@ -33,14 +33,14 @@ def find_range(ecg_reader, peakthresh = 0.7, basethresh = 0.1):
         if toggle_peak_status == 1:
             if voltage[i] < (baseline + basethresh * pos_range):
                 toggle_peak_status = 0
-    return peak_times
+        peak_vector = np.array(peak_times)
+    return peak_vector
 
 
 def main():
-    ecg_reader = read_ecg()
-    peak_vector = find_range(ecg_reader)
+    time, voltage = read_ecg()
+    peak_vector = find_range(time, voltage)
     return peak_vector
-
 
 if __name__ == "main":
     main()
