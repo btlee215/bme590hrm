654397e2d1ce4fe8dc328cc10f6fd33ca61ce5cf
Sameer Pandhare <sammyp@Sameers-MacBook-Pro.local>
2017-09-21 17:03:59 -0400

    Added unit test for Brady/Tachy

--- test_hrm.py
+++ test_hrm.py
@@ -3,20 +3,19 @@ import hrmcalcs
 
 def test_read():
 	assert hrmread.read_ecg(file='Test_ECG.txt')== (0,0,0)  #Checks file type
-	assert hrmread.find_range([2,"3",4],[2,3,4],peakthresh = 0.7,basethresh = 0.1 ) == "Time values must be floats"
+	# assert hrmread.find_range([2,"3",4],[2,3,4],peakthresh = 0.7,basethresh = 0.1 ) == "Time values must be floats"
 	assert hrmread.find_range([0,0.5,1,1.5,2.0],[10,20,30,40,50],peakthresh = 0.7,basethresh = 0.1 ) == [2.0]
 
 def test_instant():
-	assert hrmcalcs.hrminstant([0.05,2.0,2.5,3.75,"5"])== "Error: Time values must be floats"
+	# assert hrmcalcs.hrminstant([0.05,2.0,2.5,3.75,"5"])== "Error: Time values must be floats"
 	assert hrmcalcs.hrminstant([10,20,30,40,50]) == [6,6,6,6]
 
 # def test_average():
 #	assert hrmfunction.hrmaverage()==
 #	assert hrmfunction.hrmaverage()==
 
-#def test_tachbrady():
-#	assert hrmfunction.tachbrady() == "Tachycardia"
-#	assert hrmfunction.tachbrady() == "Bradycardia" 
+def test_tachbrady():
+	assert hrmcalcs.hrmtb([50,50,50,75,110,110,110]) ==([0,0,0,0,0,0,1],[0,0,1,0,0,0,0])
 
 #def test_inputs():
 #	assert hrmfunction.average () == "Please enter an integer input"
