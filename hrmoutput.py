19e8ff3dd9842257ea214d19d14903df9c6c4824
Sameer Pandhare <sammyp@Sameers-MacBook-Pro.local>
2017-09-23 15:13:18 -0400

    Few small changes

--- hrmoutput.py
+++ hrmoutput.py
@@ -1,15 +1,14 @@
-import hrmread
 import hrmcalcs
 
 def hrmprint(peakvalues, instantHR, brady,tachy):
     # Headers = ['Time (s)','Instant HR','Bradycardia','Tachycardia']
+    file = open("hrmoutput.txt", "w")
+    file.write('This is a test line')
     for row in list(zip(peakvalues, instantHR, brady, tachy)):
         out = print(str(row))
-        file = open("hrmoutput.txt", "w")
         for row in out:
             file.write("{}\n".format(row))
-            file.close()
-    return file
+    file.close()
 
 if __name__ == "main":
     peakvalues, instantHR, tachy, brady = hrmcalcs.maincalcs()
