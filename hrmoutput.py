6b3c9fecbe81e4204fb1bc80e25d38913988750a
Alice <alice@Alices-MacBook-Pro.local>
2017-09-23 16:02:24 -0400

    Fixed text file

--- hrmoutput.py
+++ hrmoutput.py
@@ -1,15 +1,18 @@
 import hrmcalcs
 
-def hrmprint(peakvalues, instantHR, brady,tachy):
-    # Headers = ['Time (s)','Instant HR','Bradycardia','Tachycardia']
+
+def hrmprint(peakvalues, instantHR, tachy, brady):
     file = open("hrmoutput.txt", "w")
-    file.write('This is a test line')
     for row in list(zip(peakvalues, instantHR, brady, tachy)):
         out = print(str(row))
-        for row in out:
-            file.write("{}\n".format(row))
+        file.write("{}\n".format(row))
     file.close()
 
-if __name__ == "main":
-    peakvalues, instantHR, tachy, brady = hrmcalcs.maincalcs()
-    hrmprint(peakvalues, instantHR,brady,tachy)
+
+def main():
+    peakvalues, instantHR, averageHR, tachy, brady = hrmcalcs.maincalcs()
+    hrmprint(peakvalues, instantHR, tachy, brady)
+
+
+if __name__ == "__main__":
+    main()
