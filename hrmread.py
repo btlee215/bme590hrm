bf5e17b5c02315e36aa22442651aaf98ece13041
Sameer Pandhare <sammyp@Sameers-MacBook-Pro.local>
2017-09-24 19:49:53 -0400

    Added all docstrings to the file

--- hrmread.py
+++ hrmread.py
@@ -59,12 +59,14 @@ def check_data_type(time, voltage):
 
 def find_range(time, voltage, peakthresh = 0.9, basethresh = 0.1):
     """
-
-    :param time:
-    :param voltage:
-    :param peakthresh:
-    :param basethresh:
-    :return:
+    This function is used to determine the range of what is considered a heartbeat. The peak and base thresholds have
+    been set in order to use the toggle_peak_status variable to detect the time values where a beat occurs.
+    :param time: Time in seconds as output by the read_ecg function
+    :param voltage: Voltage in volts as output by the read_ecg function
+    :param peakthresh: User-inputted peak threshold value. Default is set to 0.9
+    :param basethresh: User-inputted base threshold value. Default is set to 0.1
+    :return: This function will return an array titled peak_vector with the time values at which heart beats
+    were detected.
 
     """
 
@@ -92,8 +94,10 @@ def find_range(time, voltage, peakthresh = 0.9, basethresh = 0.1):
 
 def main():
     """
-
-    :return:
+    This function is run when the hrmread.py file is run and reads the file, checks the data type of the time and
+    voltage variables, and finds the time values at which a heartbeat occured.
+    :return: This function will return the value of the csv_check variable, the data_type variable and the peak vector,
+    which shows the time points at which a beat was detected
 
     """
 
