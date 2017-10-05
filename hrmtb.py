eaa48b1bb49fe99651666ba34d9e7badfcb6ffd1
btlee215 <btl15@duke.edu>
2017-10-05 15:23:14 -0400

    Removed hard-coding of thresholds

--- hrmtb.py
+++ hrmtb.py
@@ -5,16 +5,20 @@ class TachyBrady:
     def __init__(self, instantHR):
         self.instantHR = instantHR
 
-    def tb(self):
+    def tb(self, brady_limit=60, tachy_limit=100):
         tachy = []
         brady = []
         count = 0
         for i in self.instantHR:
             if count > 1:
-                if self.instantHR[count] < 60 and self.instantHR[count - 1] < 60 and self.instantHR[count - 2] < 60:
+                if self.instantHR[count] < brady_limit and \
+                                self.instantHR[count - 1] < brady_limit and \
+                                self.instantHR[count - 2] < brady_limit:
                     brady.append(1)
                     tachy.append(0)
-                elif self.instantHR[count] > 100 and self.instantHR[count - 1] > 100 and self.instantHR[count - 2] > 100:
+                elif self.instantHR[count] > tachy_limit and \
+                                self.instantHR[count - 1] > tachy_limit and \
+                                self.instantHR[count - 2] > tachy_limit:
                     brady.append(0)
                     tachy.append(1)
                 else:
