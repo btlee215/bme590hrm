f2bdf3168412cfdc297b414735ba290623567b2a
Alice <alice@Alices-MacBook-Pro.local>
2017-10-12 20:12:18 -0400

    Fixed files for test. Need to explicitly call tb method within unit test to find tachy and brady

--- hrmtb.py
+++ hrmtb.py
@@ -13,14 +13,14 @@ class TachyBrady:
         count = 0
         for i in self.instant_hr:
             if count > 1:
-                if self.instant_hr[count] < brady_limit and \
-                                self.instant_hr[count - 1] < brady_limit and \
-                                self.instant_hr[count - 2] < brady_limit:
+                if self.instant_hr[count] < self.brady_limit and \
+                                self.instant_hr[count - 1] < self.brady_limit and \
+                                self.instant_hr[count - 2] < self.brady_limit:
                     self.brady.append(1)
                     self.tachy.append(0)
-                elif self.instant_hr[count] > tachy_limit and \
-                                self.instant_hr[count - 1] > tachy_limit and \
-                                self.instant_hr[count - 2] > tachy_limit:
+                elif self.instant_hr[count] > self.tachy_limit and \
+                                self.instant_hr[count - 1] > self.tachy_limit and \
+                                self.instant_hr[count - 2] > self.tachy_limit:
                     self.brady.append(0)
                     self.tachy.append(1)
                 else:
